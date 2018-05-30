5af3361a31c9a55eade22bd3f02e77587bacfe1d
haoyinpeng <hypdreamandlove@gmail.com>
2018-05-30 22:01:25 +0800

    modify db_single to db_pool and add release

--- web.py
+++ web.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask,render_template
 import dbsession
 import db_data
 from flask import current_app,request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 #启动数据库连接池
 try:
-    db_conn = dbsession.dbconnsingle('apps','debs1apps','DERP').db_conn()
+    db_pool = dbsession.dbpool('apps','debs1apps','DERP').db_conn_pool()
 except Exception as dberror:
     print('获取数据库连接失败',dberror)
 
@@ -25,11 +27,14 @@ def sumtable_sql():
 
 def sumtable_tuple():
     res01 = db_data.get_result('666')
-    return res01.exec_sql(db_conn,sumtable_sql(),{'flag':'E'},3) 
+    t = res01.exec_sql(db_pool.acquire(),sumtable_sql(),{'flag':'E'},3)
+    db_pool.release()
+    return t
 
 @app.route('/domain')
 def index():
-    return '<h1>hello </h1> <h2> %s </h2>',sumtable_tuple()[0][5]
+    #return '<h1>hello </h1> <h2> %s </h2>',sumtable_tuple()[0][5]
+    return render_template('index.html',table = sumtable_tuple())
 
 @app.route('/context')
 def context():
