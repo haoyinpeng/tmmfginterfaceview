59158d37090a052b7de0bc7de93c07c697674449
haoyinpeng <hypdreamandlove@gmail.com>
2018-06-15 07:27:11 +0800

    add sql_set.py

--- web.py
+++ web.py
@@ -2,7 +2,7 @@
 
 from flask import Flask,render_template
 import dbsession
-import db_data
+import sql_set
 from flask import current_app,request
 #from flask_sqlalchemy import SQLAlchemy
 
@@ -16,26 +16,18 @@ try:
 except Exception as dberror:
     print('获取数据库连接池失败',dberror)
 
-
 #app_ctx = app.app_context()
 
-def sumtable_sql():
-    str_sql = ''' select * 
-                      from apps.meserpprodsum m 
-                     where process_flag = :flag '''
-    return str_sql
-
-def sumtable_tuple():
-    #res01 = db_data.get_result('666')
+def sumtable_tuple(flag,rows):
     db_sum = db_pool.acquire()
-    t = db_data.ret_sql_res(db_sum,sumtable_sql(),{'flag':'E'},3)
+    t = sql_set.ret_sql_res(db_sum,sql_set.sumtable_sql(),{'flag':flag},rows)
     db_pool.release(db_sum)
     return t
 
 @app.route('/')
 def index():
     #return '<h1>hello </h1> <h2> %s </h2>',sumtable_tuple()[0][5]
-    return render_template('index.html',table = sumtable_tuple())
+    return render_template('index.html',table = sumtable_tuple('E',10))
 
 @app.route('/context')
 def context():
